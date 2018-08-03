3c62366fba65ab361057c54a0d0266e169d744ee
brian.chen <brian.chen@infinitechain.io>
2018-08-03 10:37:26 +0800

    Add batch write in rocksdb

--- rocksdbBenchmark.go
+++ rocksdbBenchmark.go
@@ -8,7 +8,7 @@ import (
 	"github.com/tecbot/gorocksdb"
 )
 
-const num = 1000000
+const num = 10000000
 
 func main() {
 	opts := gorocksdb.NewDefaultOptions()
@@ -18,6 +18,7 @@ func main() {
 	defer db.Close()
 	ro := gorocksdb.NewDefaultReadOptions()
 	wo := gorocksdb.NewDefaultWriteOptions()
+	wb := gorocksdb.NewWriteBatch()
 	fmt.Println("Data Number: ", num)
 
 	// Sequential Write
@@ -30,7 +31,43 @@ func main() {
 		}
 	}
 	elapsed := time.Since(start)
-	fmt.Println("Sequential Write: ", elapsed)
+	fmt.Println("Sequential write time cost: ", elapsed)
+
+	// Delete data in ordered sequence
+	start = time.Now()
+	for i:=0; i < num; i++ {
+		s := strconv.Itoa(i)
+		err = db.Delete(wo, []byte(s))
+		if err != nil {
+			fmt.Println("err")
+		}
+	}
+	elapsed = time.Since(start)
+	fmt.Println("Delete data in ordered sequence time cost: ", elapsed)
+
+	// Sequential Write
+	start = time.Now()
+	for i:=0; i < num; i++ {
+		s := strconv.Itoa(i)
+		err = db.Put(wo, []byte(s), []byte("test"))
+		if err != nil {
+			fmt.Println("err")
+		}
+	}
+	elapsed = time.Since(start)
+	fmt.Println("Sequential write time cost: ", elapsed)
+
+	// Delete data in random sequence
+	start = time.Now()
+	for _, value := range rand.Perm(num) {
+		s := strconv.Itoa(value)
+		err = db.Delete(wo, []byte(s))
+		if err != nil {
+			fmt.Println("err")
+		}
+	}
+	elapsed = time.Since(start)
+	fmt.Println("Delete data in random sequence time cost: ", elapsed)
 
 	// Random Write
 	start = time.Now()
@@ -42,7 +79,7 @@ func main() {
 		}
 	}
 	elapsed = time.Since(start)
-	fmt.Println("Random Write: ", elapsed)
+	fmt.Println("Random write time cost: ", elapsed)
 
 	// Sequential Read
 	start = time.Now()
@@ -55,7 +92,7 @@ func main() {
 		}
 	}
 	elapsed = time.Since(start)
-	fmt.Println("Sequential Read: ", elapsed)
+	fmt.Println("Sequential read time cost: ", elapsed)
 
 	// Random Read
 	start = time.Now()
@@ -68,5 +105,35 @@ func main() {
 		}
 	}
 	elapsed = time.Since(start)
-	fmt.Println("Random Read: ", elapsed)
+	fmt.Println("Random read time cost: ", elapsed)
+
+	// Batch write in ordered sequence
+	wb.Clear()
+	start = time.Now()
+	for i:=0; i < num; i++ {
+		s := strconv.Itoa(i)
+		wb.Put([]byte(s), []byte("test"))
+	}
+	err = db.Write(wo, wb)
+	if err != nil {
+		fmt.Println("err")
+	}
+	elapsed = time.Since(start)
+	wb.Clear()
+	fmt.Println("Batch write in ordered sequence time cost: ", elapsed)
+
+	// Batch Write in random sequence
+	wb.Clear()
+	start = time.Now()
+	for _, value := range rand.Perm(num) {
+		s := strconv.Itoa(value)
+		wb.Put([]byte(s), []byte("test"))
+	}
+	err = db.Write(wo, wb)
+	if err != nil {
+		fmt.Println("err")
+	}
+	elapsed = time.Since(start)
+	wb.Clear()
+	fmt.Println("Batch write in random sequence time cost: ", elapsed)
 }
